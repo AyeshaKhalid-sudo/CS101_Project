f108b73e8467ed5456de78fe28aeb59ca78c1f79
Aiza-Humayun <humayunaiza414@gmail.com>
2024-12-14 17:30:55 +0500

    Update CS101 Semester Project.cpp

--- CS101 Semester Project.cpp
+++ CS101 Semester Project.cpp
@@ -8,8 +8,6 @@ using namespace std;
 void info_check();
 int buy_ticket1();
 int buy_ticket2();
-void moviedetails(const string &title,const string &genre,const string &time,const string  &ratings,const string &summary);
-void showingtoday();
 
 void info_check(){
 	
@@ -34,7 +32,7 @@ void info_check(){
 			getline(cin,name);				//call aleena file and match
 			cout<<endl;
 			
-			ifstream read("User.txt");
+		/*	ifstream read("User.txt");
 			getline(read , file_name);
 			if (name != file_name){
 				
@@ -42,10 +40,10 @@ void info_check(){
 				cout<<"\t\t\t\t Press any key to conyinue :"<<endl;
 				getch();        //call main 
 			}
-			else if (name == file_name){
+			else if (name == file_name){*/
 				
 				buy_ticket1();				//call function
-			}
+			
 		}
 		else if(flag2=="NO"||flag2=="No"||flag2=="no"){
 			
@@ -63,18 +61,27 @@ void info_check(){
 		//call main function
 	}
 }
+
 int buy_ticket1(){
 		
 		system("cls");
+		int tickets, choice1, choice3;
+			
 		cout<<endl<<endl;
-		cout<<"\t\t\t\t Please choose your prefered movie :"<<endl<<endl;
-		
-	//	showingtoday();	
-		
-		int tickets, choice1;
+		cout<<"\t\t\t\t\t\t SHOWING TODAY"<<endl<<endl<<endl;
+		cout <<"\t\t\t\t 1.The Shawshank Redemption"<<"  "<<"ratings: 9/10"<<"\t 8:30  AM"<<endl;
+	   	cout <<"\t\t\t\t 2.Inception"<<"                  "<<"ratings: 8/10" <<"\t 10:30 AM"<<endl; 
+	   	cout <<"\t\t\t\t 3.Parasite"<<"                   "<<"ratings: 7/10"<<"\t 12:20 PM"<<endl;
+		cout <<"\t\t\t\t 4.The Dark Knight"<<"            "<<"ratings: 9/10"<<"\t 3:30  PM"<<endl;
+		cout <<"\t\t\t\t 5.Forrest Gump"<<"               "<<"ratings: 8/10"<<"\t 5:10  PM"<<endl<<endl<<endl;
+		cout<<"\t\t\t\t Please choose your prefered movie (1-5) : ";
+		cin >> choice3;
+		cout<<endl<<endl;
+	
 		cout<<"\t\t\t\t Enter number of ticket you would like to purchase : ";
 		cin>>tickets;
-		//system("cls");
+		system("cls");
+		cout<<endl<<endl;
 		cout<<"\t\t\t\t\t\t Seating Arrangement"<<endl;
 		cout<<endl;
 		cout<<"\t\t\t\t\t      1  2  3  4  5  6  7  8  9  10"<<endl;
@@ -117,12 +124,12 @@ int buy_ticket1(){
 				cout<<"\t\t\t\t Invalid choice"<<endl;
 				cout<<"\t\t\t\t Please choose again "<<endl;
 				buy_ticket1();
-				break;//use while instead
-				
+				break;
 	}	
 	buy_ticket2();
 	return tickets;
 	return choice1;
+	return choice3;
 }
 int buy_ticket2(){
 	
@@ -160,90 +167,9 @@ int buy_ticket2(){
 				buy_ticket2();
 				break;
     }
+    //call ayesha's function
     return choice2;
 }
-
-//AREEJ
-void moviedetails(const string &title,const string &genre,const string &time,const string  &ratings,const string &summary){   
-
-	 //moviedetailsfunction
-
-	bool buy;
-	cout <<"\t\t****************************************************************\n";
-    cout <<"\n\t\t========================================================\n";
-	cout <<"\n\n\t\t\t"<<title<<endl;
-	cout <<"\n\t\t========================================================\n";
-	cout <<"\t\t****************************************************************\n";
-	cout <<"\t\t\t\t\t\t\t\t\t\t\t\t ";
-	
-	cout <<"\n\t\t Genre:  "<<genre<<endl;
-	cout <<"\n\t\t Time:"<<time<<endl;
-	cout <<"\n\t\t Ratings: "<<ratings <<endl;
-	cout <<"\n\t\t Summary:" <<summary<<endl;
-	cout <<"\t\t\tDo you want to buy ticket?( 0 for no,1 for yes)"<<endl;//BUYnow
-	
-	cin >> buy;
-	if(buy){
-		cout <<"\t\t\tThankyou for buying ticket.."<<endl;
-	}
-	else{
-		cout <<"\t\tOpps!"<<endl;
-	}
-}
-
-void showingtoday() {				//AREEJ
-	
-	int option=0;
-  
-    do{
-
-    cout <<"\t\t\t\t****************************************************************";
-    cout <<"\n\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-   	cout <<"\n\n\t\t\t\t\t\t\t SHOWING TODAY\n\n"<<endl;
-   	cout <<"\n\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-   	cout <<"\t\t\t\t****************************************************************";	
-   	cout <<"\n\n";
-	cout <<"\t\t\t\t\t\t\t\t\t\t\t\t"<<endl;
-
-   	cout <<"\t\t\t\t 1.The Shawshank Redemption"<<"  "<<"ratings: 9/10"<<"\t 8:30  AM"<<endl;
-   	cout <<"\t\t\t\t 2.Inception"<<"                  "<<"ratings: 8/10" <<"\t 10:30 AM"<<endl; 
-   	cout <<"\t\t\t\t 3.Parasite"<<"                   "<<"ratings: 7/10"<<"\t 12:20 PM"<<endl;
-	cout <<"\t\t\t\t 4.The Dark Knight"<<"            "<<"ratings: 9/10"<<"\t 3:30  PM"<<endl;
-	cout <<"\t\t\t\t 5.Forrest Gump"<<"               "<<"ratings: 8/10"<<"\t 5:10  PM"<<endl<<endl<<endl;
-	cout <<"\t\t\t\t****************************************************************\n";
-	cout <<"\t\t\t\t****************************************************************\n"<<endl;
-    cout << "\t\t\t\tPick Movie (1-5 /0 for exit to menu): "; 
-    cin>>option;
-    
-     switch(option){//for checking multiple cases 
-		case 1 :
-		moviedetails("The Shawshank Redemption","DRAMA","8:30-10:30PM","9/10","Man escapes prison through hope, friendship, and perseverance over decades.");
-			break;//function calling moviedetails
-		case 2:
-		  moviedetails("INCEPTION","Sci-Fi, Thriller","10:30-12:00PM","8/10","Thieves plant ideas in dreams, blurring reality with layered subconscious.");
-			break;
-		case 3:
-	       moviedetails("Parasite","Sci-Fi, Thriller","12:30-2:00PM","7/10","Poor family infiltrates rich household, leading to shocking class conflict.");
-			break;
-		case 4:
-	       moviedetails("The Dark Knight","Action, Crime, Thriller","2:30-4:30PM","9/10","Batman battles Joker in a chaotic fight for Gothamâ€™s soul.");
-			break;
-		case 5:
-			moviedetails("Forrest Gump"," Drama, Romance","5-6:30PM","8/10","Simple man witnesses history, finding love and purpose through lifeâ€™s challenges.");
-	      break;
-	    case 0:
-	    	cout <<"\t\t\tExit to main menu"<<endl;
-	    	break;//exit case
-		default: 
-		cout <<"\t\t\t\tChoose from (1-5).Please try again!"<<endl;
-		break;
- 	} 
- 	
-	} while(option!=0);
-	
- 
-}
-
 
 int main(){
 	
